5c957da94b39b0c299afef079b36e6a5bf8f710a
swozn <arpheno@gmail.com>
2016-06-23 08:42:50 +0200

    basic functionality

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-from itertools import zip_longest
+from itertools import zip_longest, accumulate
 
-from processing import symbolize
-from latex import chord
+from processing import symbolize, ultimate_guitar, canonize_header, split_song, wrap
+from latex import chord, songbook_header
 
-def convert_chords(c,t):
-    return symbolize(c), t
+
+def convert_chords(c, t):
+    result = list(symbolize(c))
+    acc = 0, *list(accumulate(len(x) for x in result)),999
+    trans = [t[start:end] for start,end in zip(acc,acc[1:])]
+    print(result)
+    print(trans)
+    return result, trans
+
+
 def separate_lines(blob):
     yield from zip(blob.splitlines()[::2], blob.splitlines()[1::2])
+
 
 def process_verse(blob):
     res = [zip_longest(*convert_chords(*line), fillvalue=" ") for line in separate_lines(blob)]
     res = [[chord(a, b) if not a.isspace() else b for a, b in line] for line in res]
-    return  "\n".join("".join(e) for e in res)
-if __name__ == "__main__":
-    pass
+    return "\n\n".join("".join(e) for e in res)
 
+
+if __name__ == "__main__":
+    one_call = 'https://tabs.ultimate-guitar.com/c/charlie_puth/one_call_away_crd.htm'
+    wonderwall="https://tabs.ultimate-guitar.com/o/oasis/wonderwall_ver2_crd.htm"
+    title, artist, song = ultimate_guitar(wonderwall)
+    sections = split_song(song, r'(\[.*?\])')
+    sections = [(canonize_header(header), process_verse(content)) for header, content in sections]
+    sections = [wrap(header, content) for header, content in sections]
+    body = "\n".join(sections)
+    latex = songbook_header()+wrap("document",wrap("song", body, title, "", artist, artist, "", ""))
+    with open(" - ".join([artist,title])+".tex","w") as f:
+        f.write(latex)
--- processing.py
+++ processing.py
@@ -1,4 +1,8 @@
+import re
 from itertools import groupby
+
+from bs4 import BeautifulSoup
+from requests import get
 
 
 def flatten(l):
@@ -13,7 +17,66 @@ def join_chord_and_leave_spaces(grouped):
     yield from (["".join(l)] if any(not e.isspace() for e in l) else l for l in grouped)
 
 
+def extend_single_chords(symbols):
+    skip = False
+    for symbol in symbols:
+        if not symbol.isspace() and len(symbol)==1:
+            yield symbol + " "
+            skip = True
+        if not skip:
+            yield symbol
+        else:
+            skip = False
+
+
+
 def symbolize(line_with_chords):
     grouped = groupby_spaces(line_with_chords)
     symbols = join_chord_and_leave_spaces(grouped)
-    return flatten(symbols)
+    flat = flatten(symbols)
+    symbols = extend_single_chords(flat)
+    return symbols
+
+
+
+def split_song(blob, grammar):
+    parts = re.split(grammar, blob)[1:]  # Element 0 is empty
+    parts = list(zip(parts[::2], parts[1::2]))
+    return parts
+
+
+def canonize_header(header):
+    """
+    >>> canonize_header('[verse 3]')
+    '[SBVerse]'
+    """
+    sections = {
+        'intro': '[SBIntro]',
+        'verse': '[SBVerse]',
+        'bridge': '[SBBridge]',
+        'outro': '[SBEnd]',
+        'chorus': '[SBChorus]',
+    }
+    for key in sections:
+        if key in header.lower():
+            print(sections[key])
+
+            return sections[key]
+    return '[SBOpGroup]'
+
+
+def wrap(header, section, *args):
+    """
+    >>> wrap('[SBVerse]',"x")
+    '\\\\begin{SBVerse}\\nx\\\\end{SBVerse}\\n'
+    """
+    wrapper = header.strip("[]").strip()
+    if args:
+        section = "".join("{" + arg + "}" for arg in args) + '\n' + section
+    return '\\begin{%s}\n%s\\end{%s}\n' % (wrapper, section, wrapper)
+
+
+def ultimate_guitar(url):
+    content = get(url).content
+    soup = BeautifulSoup(content, "html.parser")
+    return soup.h1.text, soup.find("div", {"class": "t_autor"}).a.text, soup.find_all("pre")[-1].text
